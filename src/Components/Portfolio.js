5008ec313db2887588108c35f0f7ed9f8bf561a7
Aiman <aimanshakeel1008@gmail.com>
2020-09-09 14:06:34 +0530

    made some changes in UI

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Tilt from "react-parallax-tilt";
 
 class Portfolio extends Component {
   render() {
@@ -7,20 +8,22 @@ class Portfolio extends Component {
         var projectImage = "images/portfolio/" + projects.image;
         return (
           <div key={projects.title} className="columns portfolio-item">
-            <div className="item-wrap">
-              <a href={projects.url} title={projects.title}>
-                <img alt={projects.title} src={projectImage} />
-                <div className="overlay">
-                  <div className="portfolio-item-meta">
-                    <h5>{projects.title}</h5>
-                    <p>{projects.category}</p>
+            <Tilt>
+              <div className="item-wrap">
+                <a href={projects.url} title={projects.title}>
+                  <img alt={projects.title} src={projectImage} />
+                  <div className="overlay">
+                    <div className="portfolio-item-meta">
+                      <h5>{projects.title}</h5>
+                      <p>{projects.category}</p>
+                    </div>
                   </div>
-                </div>
-                <div className="link-icon">
-                  <i className="fa fa-link"></i>
-                </div>
-              </a>
-            </div>
+                  <div className="link-icon">
+                    <i className="fa fa-link"></i>
+                  </div>
+                </a>
+              </div>
+            </Tilt>
           </div>
         );
       });
